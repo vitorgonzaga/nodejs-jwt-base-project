782573e5b8bdbdaac6007531cdbaa606c5b2d116
Rogério Munhoz <roz@rjmunhoz.me>
2021-07-12 16:39:39 -0300

    minor changes
    - adiciona imagem sobre fluxo do jwt
    - altera URL padrão do mongo para utilizar `localhost`

--- models/connection.js
+++ models/connection.js
@@ -1,22 +1,21 @@
 const { MongoClient } = require('mongodb');
 
 const OPTIONS = {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-}
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
 
-const MONGO_DB_URL = 'mongodb://127.0.0.1:27017';
+const MONGO_DB_URL = 'mongodb://localhost:27017';
 
 let db = null;
 
 const connection = () => {
-    return db
+  return db
     ? Promise.resolve(db)
-    : MongoClient.connect(MONGO_DB_URL, OPTIONS)
-    .then((conn) => {
-    db = conn.db('jwt_exercise');
-    return db;
-    })
+    : MongoClient.connect(MONGO_DB_URL, OPTIONS).then((conn) => {
+        db = conn.db('jwt_exercise');
+        return db;
+      });
 };
 
 module.exports = connection;
