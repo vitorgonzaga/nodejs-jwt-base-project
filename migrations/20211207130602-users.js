3d3f990cf182611517f617d0e5a6d62c27e136cf
Felipe Muller <felipeavmuller@gmail.com>
2021-12-07 10:25:37 -0300

    Cria Models e Seeders

--- migrations/20211207130602-users.js
+++ migrations/20211207130602-users.js
@@ -9,7 +9,7 @@ module.exports = {
         primaryKey: true,
         type: Sequelize.INTEGER,
       },
-      name: {
+      username: {
         allowNull: false,
         type: Sequelize.STRING,
       },
