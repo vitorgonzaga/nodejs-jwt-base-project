ae2890b18b5f44f38e0a0a2008fe35f219d7c0ab
Felipe Muller <felipeavmuller@gmail.com>
2021-12-07 10:12:36 -0300

    Create migration

---
+++ migrations/20211207130602-users.js
@@ -0,0 +1,25 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    const User = queryInterface.createTable("Users", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      password: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+    });
+  },
+  down: async (queryInterface, _Sequelize) => {
+    queryInterface.dropTable("Users");
+  }
+};
